3e448688f436f91efb3e893ff9b90fa152caa894
wenchong <wchung.huang@gmail.com>
2024-01-31 18:20:56 +0800

    update after class

--- Lecture 7 - computational geometry/src/minCircle.cpp
+++ Lecture 7 - computational geometry/src/minCircle.cpp
@@ -1,89 +1,91 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const double eps = 1e-13;
+const int N = 100010;
 struct Point{
-	double x, y;
-	Point(double x=0, double y=0): x(x), y(y) {}
-} Q[100010];
+    double x, y;
+    Point(double x=0, double y=0): x(x),y(y) {}
+    double len() const{
+        return sqrt(x * x + y * y);
+    }
+};
+#define Vector Point
+Point p[N];
 int n;
 
-inline double sqr(const double &x){return x*x;}
-Point operator - (Point a,Point b){return Point(a.x-b.x,a.y-b.y);}
+Vector operator * (const double &a, const Point &b){
+    return Vector(a*b.x, a*b.y);
+}
+Vector operator + (const Point &a, const Point &b){
+    return Vector(a.x+b.x, a.y+b.y);
+}
+Vector operator - (const Point &a, const Point &b){
+    return Vector(a.x-b.x, a.y-b.y);
+}
 
 int dcmp(double x){
-    if(fabs(x) <= eps) return 0;
+    if(fabs(x) < 1e-12) return 0;
     else if(x<0) return -1;
     else return 1;
 }
 
-double Length(const Point &a){return sqrt(a.x*a.x+a.y*a.y);}
+bool incircle(const Point &o, const double &r, const Point &p){
+    return dcmp((o-p).len() - r) <= 0;
+}
+
+double sqr(const double &x){return x * x;}
 
 void geto(const Point &a, const Point &b, const Point &c, Point &ans, double &r) {
-  double a1, a2, b1, b2, c1, c2;
-  a1 = 2 * (b.x - a.x), b1 = 2 * (b.y - a.y),
-  c1 = sqr(b.x) - sqr(a.x) + sqr(b.y) - sqr(a.y);
-  a2 = 2 * (c.x - a.x), b2 = 2 * (c.y - a.y),
-  c2 = sqr(c.x) - sqr(a.x) + sqr(c.y) - sqr(a.y);
-  if (dcmp(a1) == 0) {
-    ans.y = c1 / b1;
-    ans.x = (c2 - ans.y * b2) / a2;
-  } else if (dcmp(b1) == 0) {
-    ans.x = c1 / a1;
-    ans.y = (c2 - ans.x * a2) / b2;
-  } else {
-    ans.x = (c2 * b1 - c1 * b2) / (a2 * b1 - a1 * b2);
-    ans.y = (c2 * a1 - c1 * a2) / (b2 * a1 - b1 * a2);
-  }
-  r = Length(a-ans);
+    double a11, a12, a21, a22, b1, b2;
+    a11 = 2 * (b.x - a.x); a12 = 2 * (b.y - a.y);
+    a21 = 2 * (c.x - a.x); a22 = 2 * (c.y - a.y);
+    b1 = sqr(b.x) - sqr(a.x) + sqr(b.y) - sqr(a.y);
+    b2 = sqr(c.x) - sqr(a.x) + sqr(c.y) - sqr(a.y);
+    ans.x = (b1 * a22 - b2 * a12) / (a11 * a22 - a12 * a21);
+    ans.y = (a11 * b2 - a21 * b1) / (a11 * a22 - a12 * a21);
+    r = (a-ans).len();
 }
 
-bool in_circle(const Point &A, const Point &O, const double &r){
-    return dcmp(Length(A-O) - r) <= 0;
-}
-
-// 计算 MC(Q[i],Q[j];  Q[1],...,Q[j-1])
-void getMC_fix2(int i, int j, Point &ans, double &r){
-    ans.x = 0.5 * (Q[i].x + Q[j].x);
-    ans.y = 0.5 * (Q[i].y + Q[j].y);
-    r = Length(Q[i] - Q[j]) / 2;
-
+// 求 MC(P[i],P[j]; P[1],...,P[j-1])
+void mc_fix2(int i, int j, Point &ans, double &r){
+    ans = 0.5 * (p[i] + p[j]);
+    r = (p[i]-p[j]).len() / 2;
     for(int k = 1; k < j; k++){
-        if(in_circle(Q[k], ans, r)) continue;
-        geto(Q[i], Q[j], Q[k], ans, r);
+        if(incircle(ans, r, p[k])) continue;
+        // 根据引理3，MC(P[i],P[j]; P[1],...,P[k]) = MC(P[i],P[j],P[k]; P[1],...,P[k-1])
+        geto(p[i], p[j], p[k], ans, r);
     }
 }
 
-// 计算 MC(Q[i];  Q[1],...,Q[i-1])
-void getMC_fix1(int i, Point &ans, double &r){
-    ans.x = 0.5 * (Q[i].x + Q[1].x);
-    ans.y = 0.5 * (Q[i].y + Q[1].y);
-    r = Length(Q[i] - Q[1]) / 2;
-
+// 求 MC(P[i]; P[1],...,P[i-1])
+void mc_fix1(int i, Point &ans, double &r){
+    ans = 0.5 * (p[i] + p[1]);
+    r = (p[i]-p[1]).len() / 2;
     for(int j = 2; j < i; j++){
-        if(in_circle(Q[j], ans, r)) continue;
-        getMC_fix2(i, j, ans, r);
+        if(incircle(ans, r, p[j])) continue;
+        // 根据引理2，MC(P[i]; P[1],...,P[j]) = MC(P[i],P[j]; P[1],...,P[j-1])
+        mc_fix2(i, j, ans, r);
     }
 }
 
-// 计算 MC(空;  Q[1],...,Q[n])
-void getMC_fix0(Point &ans, double &r){
-    ans = Q[1];
-    r = 0;
+// 求 MC(空; P[1],...,P[n])
+void mc_fix0(Point &ans, double &r){
+    ans = p[1]; r = 0;
     for(int i = 2; i <= n; i++){
-        if(in_circle(Q[i], ans, r)) continue;
-        getMC_fix1(i, ans, r);
+        if(incircle(ans, r, p[i])) continue;
+        // 根据引理1，MC(空; P[1],...,P[i]) = MC(P[i]; P[1],...,P[i-1])
+        mc_fix1(i, ans, r);
     }
 }
 
 int main(){
     scanf("%d", &n);
     for(int i = 1; i <= n; i++)
-        scanf("%lf%lf", &Q[i].x, &Q[i].y);
-    random_shuffle(Q+1, Q+1+n);
+        scanf("%lf%lf", &p[i].x, &p[i].y);
+    random_shuffle(p+1, p+1+n);
     Point ans;
     double r;
-    getMC_fix0(ans, r);
+    mc_fix0(ans, r);
     printf("%.10lf\n%.10lf %.10lf\n", r, ans.x, ans.y);
     return 0;
 }
